51652078ed3cc77c93061d43649308792c242703
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2024-12-08 23:49:19 -0700

    Added Jump flag to necessary modules to Stall and prevent fetching next instruction until Jump is processed. Now goes through private test cases entirely

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -17,6 +17,7 @@ module HazardDetectionUnit (
     input [4:0]  EX_Rd,
     input [4:0]  MEM_RegDst,
     input [1:0] ForwardA,
+    input       ID_Jump,
     output reg   ID_Stall,
     output reg   Flush_IF_ID
 );
@@ -40,7 +41,7 @@ module HazardDetectionUnit (
         MEMStall = 0;
         BranchStall = 0;
     
-        if (ID_Branch) begin
+        if (ID_Branch || ID_Jump) begin
             BranchStall = 1;
         end
 
@@ -71,6 +72,7 @@ module HazardDetectionUnit (
             end 
 //            if(BranchStall) begin
 //                ID_Stall <= 1;
+//                Flush_IF_ID <= 1; // Flush IF/ID
 //            end
     end
     
